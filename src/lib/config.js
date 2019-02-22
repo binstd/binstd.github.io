bd332baf90a627e20f5fe74ddeac5648d8af21fe
ethluz <lvxing67@126.com>
2019-02-22 11:12:12 +0800

    开发环境切换

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,3 +1,10 @@
 // export const server_url = 'https://api.binstd.com';
+let server_url = 'https://api.binstd.com';
+if (process.env.NODE_ENV === 'production') {
+    server_url = 'https://api.binstd.com';
+} else {
+    server_url = 'http://127.0.0.1:3000';
+}
 
-export const server_url = 'http://127.0.0.1:3000';
+console.log('server_url:',server_url);
+export {server_url};
