b221889690bd44ff7f267f4f0cb0d4de3d3d846d
ethluz <lvxing67@126.com>
2019-03-05 09:55:43 +0800

    server url = heroku

--- src/lib/config.js
+++ src/lib/config.js
@@ -4,7 +4,8 @@ if (process.env.NODE_ENV === 'dev') {
     // server_url = 'https://binstd.herokuapp.com';
     server_url = 'http://127.0.0.1:3000'
 } else {
-    server_url = 'https://api.binstd.com';
+    server_url = 'https://binstd.herokuapp.com';
+    // server_url = 'https://api.binstd.com';
 }
 
 console.log('server_url:',server_url);
