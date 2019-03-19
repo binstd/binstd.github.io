7003bae318f2264dc5f432459bc95e9c878a808e
ethluz <lvxing67@126.com>
2019-03-19 19:20:49 +0800

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c6caa9edc6aad485b619.js"
+    "url": "webpack-runtime-cd0188926d10cc260121.js"
   },
   {
     "url": "app-c646f1ba03696a92a6d3.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "006caa5452af4f85d09801260d1181a6"
+    "revision": "b9e100ad704af26897b9781e1619ce6f"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json",
